a3f844766d058940e49dedcc7ae39d155d223390
Alexey Gvozdev <lesha_chess@mail.ru>
2017-04-03 14:54:30 +0300

    hello world

---
+++ hello.js
@@ -0,0 +1,8 @@
+ var http = require("http");
+
+ http.createServer(function (request, response) {
+     response.writeHead(200, {"Contett-Type": "text/arial"});
+     response.end("Hello World!");
+ }).listen(8125);
+
+ console.log("Server running at http://127.0.0.1:8125/");
