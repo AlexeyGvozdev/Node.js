863d3abcd99a3d2f865fc044163d3443e2765005
Alexey Gvozdev <lesha_chess@mail.ru>
2017-04-16 14:00:00 +0300

    path.normalaze

--- web/ServerStatFiles/Server.js
+++ web/ServerStatFiles/Server.js
@@ -1,11 +1,13 @@
 var http = require('http'),
     fs = require('fs'),
-    base = '\.',
+    url = require('url'),
+    path = require('path'),
     mime = require('mime');
 
+    var base = '.';
+
 http.createServer(function(req, res) {
-    var pathname = base + req.url;
-    var type = mime.lookup(pathname);
+    var pathname = path.normalize(base + req.url);
     console.log(pathname);
 
     fs.stat(pathname, function(err, stats) {
@@ -14,22 +16,28 @@ http.createServer(function(req, res) {
             res.writeHead(404);
             res.write('Resource mossing 404\n');
             res.end();
-        } else {
+        } else if(stats.isFile()){
+            // тип контента
+            var type = mime.lookup(pathname);
+            console.log(type);
             res.setHeader('Content-Type', type);
-
+            
             // Создание и перенаправление потока для чтения
             var file = fs.createReadStream(pathname);
-
             file.on('open', function() {
                 res.statusCode = 200;
                 file.pipe(res);
             }); 
             file.on('error', function(err){
                 console.log(err);
-                res.writeHead(403);
-                res.write('file missing or permission problem');
+                res.statusCode = 403;
+                res.write('file permission');
                 res.end();
             });
+        } else {
+            res.writeHead(403);
+            res.write('Directory access is forbidden');
+            res.end();
         }
     });
 }).listen(8124);
